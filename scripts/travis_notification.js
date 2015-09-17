1529713405f148bb64577788e1ad55fa2fdd8f7c
Ryohei Ueda <garaemon@gmail.com>
2015-09-17 15:37:28 +0900

    Add debug messages

--- scripts/travis_notification.js
+++ scripts/travis_notification.js
@@ -8,13 +8,16 @@ module.exports = function(robot) {
             if (msg.subtype == "bot_message") { // bot_id = B08QP3RRT?
                 console.log(msg);
                 if (msg.attachments.length > 0) {
+                    console.log("have attachments");
                     if (msg.attachments[0].text) {
+                        console.log("have texts");
                         // msg.attachments[0].text should be like
                         // "Build <https://travis-ci.org/jsk-ros-pkg/jsk_planning/builds/80766954|#76> (<https://github.com/jsk-ros-pkg/jsk_planning/pull/28|b214435>) of jsk-ros-pkg/jsk_planning@master in PR <https://github.com/jsk-ros-pkg/jsk_planning/pull/28|#28> by Ryohei Ueda passed in 13 min 26 sec"
                         var urls = msg.attachments[0].text.match(/<[^<>]*>/g);
                         // urls[0] - travis lnk
                         // urls[1] and urls[2] - github link
                         if (urls.length == 3) {
+                            console.log("have urls");
                             if (msg.attachments[0].text.match(/by (.*) passed/)) {
                                 var name = msg.attachments[0].text.match(/by (.*) passed/)[1];
                                 robot.send({

68b9cb810dcea4dd5b512f51185be22361f470d6
Ryohei Ueda <garaemon@gmail.com>
2015-09-17 17:41:10 +0900

    Better regular expression to detect equation

--- scripts/aliases.js
+++ scripts/aliases.js
@@ -24,7 +24,7 @@ module.exports = function(robot) {
         }
     }
     // Special case for calculation
-    robot.hear(/^[pie0-9\^\.\*\+\/\(\)\s]+$/, function(res) {
+    robot.hear(/^(pi|[e0-9\^\.\*\+\/\(\)\s]|sin|cos|tan)+$/, function(res) {
         // looks like equation
         res.message.text = res.robot.name + ' calculate ' + res.message.text;
     });

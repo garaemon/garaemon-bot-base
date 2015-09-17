79a05928826e4307b5e39511a8afceb70582babc
Ryohei Ueda <garaemon@gmail.com>
2015-09-17 14:20:34 +0900

    send only if build passed

--- scripts/travis_notification.js
+++ scripts/travis_notification.js
@@ -8,10 +8,17 @@ module.exports = function(robot) {
             if (msg.subtype == "bot_message") { // bot_id = B08QP3RRT?
                 if (msg.attachments.len > 0) {
                     if (msg.attachments[0].text) {
+                        // msg.attachments[0].text should be like
+                        // Build <https://travis-ci.org/jsk-ros-pkg/jsk_planning/builds/80766954|#76> (<https://github.com/jsk-ros-pkg/jsk_planning/pull/28|b214435>) of jsk-ros-pkg/jsk_planning@master in PR <https://github.com/jsk-ros-pkg/jsk_planning/pull/28|#28> by Ryohei Ueda passed in 13 min 26 sec
                         var urls = msg.attachments[0].text.match(/<[^<>]*>/g);
+                        // urls[0] - travis lnk
+                        // urls[1] and urls[2] - github link
                         if (urls.len == 3) {
-                            var name = msg.attachments[0].text.match(/by (.*) passed/)[1];
-                            robot.send({room: channel}, util.format("@%s Build finished by %s", owener, name));
+                            if (msg.attachments[0].text.match(/by (.*) passed/)) {
+                                var name = msg.attachments[0].text.match(/by (.*) passed/)[1];
+                                robot.send({room: channel},
+                                           util.format("@%s Build finished by %s", owener, name));
+                            }
                         }
                     }
                 }

d19ee1c19850ff5ae7bd26b638263014463dbe22
Ryohei Ueda <garaemon@gmail.com>
2015-09-17 15:30:28 +0900

    Fix .len -> .length

--- scripts/travis_notification.js
+++ scripts/travis_notification.js
@@ -7,14 +7,14 @@ module.exports = function(robot) {
         robot.adapter.client.on("raw_message", function(msg) {
             if (msg.subtype == "bot_message") { // bot_id = B08QP3RRT?
                 console.log(msg);
-                if (msg.attachments.len > 0) {
+                if (msg.attachments.length > 0) {
                     if (msg.attachments[0].text) {
                         // msg.attachments[0].text should be like
                         // "Build <https://travis-ci.org/jsk-ros-pkg/jsk_planning/builds/80766954|#76> (<https://github.com/jsk-ros-pkg/jsk_planning/pull/28|b214435>) of jsk-ros-pkg/jsk_planning@master in PR <https://github.com/jsk-ros-pkg/jsk_planning/pull/28|#28> by Ryohei Ueda passed in 13 min 26 sec"
                         var urls = msg.attachments[0].text.match(/<[^<>]*>/g);
                         // urls[0] - travis lnk
                         // urls[1] and urls[2] - github link
-                        if (urls.len == 3) {
+                        if (urls.length == 3) {
                             if (msg.attachments[0].text.match(/by (.*) passed/)) {
                                 var name = msg.attachments[0].text.match(/by (.*) passed/)[1];
                                 robot.send({

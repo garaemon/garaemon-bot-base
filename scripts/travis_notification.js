e55d56fc09ecc4f936ae4f2071837338f51befca
Ryohei Ueda <garaemon@gmail.com>
2015-09-17 13:22:49 +0900

    Add travis_notification.js

---
+++ scripts/travis_notification.js
@@ -0,0 +1,11 @@
+var util = require("util");
+
+module.exports = function(robot) {
+    robot.listen(function(msg) {
+        return msg.user.name == "Travis CI";
+    }, {}, function(res) {
+        channel = process.env["BOT_CHANNEL"] || "#owerner";
+        owener = process.env["OWENER_NAME"] || "owerner";
+        robot.send({room: channel}, util.format("@%s New travis build!", owener));
+    });
+}

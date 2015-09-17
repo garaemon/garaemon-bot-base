e28a090237aa6147c0ee1b535ee5fdfe71e3786c
Ryohei Ueda <garaemon@gmail.com>
2015-09-17 13:46:07 +0900

    Debug msg

--- scripts/travis_notification.js
+++ scripts/travis_notification.js
@@ -2,6 +2,7 @@ var util = require("util");
 
 module.exports = function(robot) {
     robot.listen(function(msg) {
+        console.log(msg);
         return msg.user.name == "Travis CI";
     }, {}, function(res) {
         channel = process.env["BOT_CHANNEL"] || "#owerner";
